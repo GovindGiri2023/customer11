492de6db9402469050b42fb3d98bb067d31aa997
govindgiri2021 <govindgiri2021@gmail.com>
2022-05-09 12:38:36 +0530

    Initial commit

--- gitbranch.sh
+++ gitbranch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     git init
-    git remote add origin git git govindgiri2021/sample
+    git remote add origin git govindgiri2021/sample
     git add .
     git commit -m "Initial commit"
     git push -u origin master
