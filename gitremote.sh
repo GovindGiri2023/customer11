50d24bbe0c0ec1b977f791c62e4048d936442302
govindgiri2021 <govindgiri2021@gmail.com>
2022-05-09 18:15:33 +0530

    initial commit

--- gitremote.sh
+++ gitremote.sh
@@ -3,7 +3,7 @@ git init .
 git add --all
 git commit -m"initial commit"
 
-git remote add origin $1
+git remote add origin https://github.com/govindgiri2021/myDirName.git
 git branch develop
 #git push origin --all
 #git push origin --tags
