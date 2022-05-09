627099a57ac0907cc2dcb435f9761ae08b84e57c
govindgiri2021 <govindgiri2021@gmail.com>
2022-05-09 19:06:04 +0530

    First commit

--- gitremote.sh
+++ gitremote.sh
@@ -1,12 +1,11 @@
-Username: govindgiri2021
-Password: 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
+
 git config --global user.name $1
 git config --global user.email $2
 git init
 git add .
 git status
 git commit -m "First commit"
-git remote add origin https://$Username:$Password@github.com/govindgiri2021/myDirName.git
+git remote add origin https://@github.com/govindgiri2021/myDirName.git
 git checkout -b govind
-git push https://$Username:$Password@github.com/govindgiri2021/myDirName.git govind
+git push https://govindgiri2021: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@github.com/govindgiri2021/myDirName.git
 
