1bc7034681614b5541b214a9e86b79d8c522bc5d
govindgiri2021 <govindgiri2021@gmail.com>
2022-05-10 12:17:29 +0530

    First commit

--- gitremote.sh
+++ gitremote.sh
@@ -5,7 +5,7 @@ git init
 git add .
 git status
 git commit -m "First commit"
-git remote add origin https://@github.com/govindgiri2021/myDirName.git
+git remote add origin https://@github.com/govindgiri2021/testing11.git
 git checkout -b govind
-git push https://govindgiri2021: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@github.com/govindgiri2021/myDirName.git
+git push https://govindgiri2021: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@github.com/govindgiri2021/testing11.git
 
